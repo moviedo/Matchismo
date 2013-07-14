ecc5473fd9fcba80f5e93825f22f82ab04b7969f
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2013-07-14 16:40:45 -0400

    Added Class PlayingCard.

---
+++ Matchismo/PlayingCard.m
@@ -0,0 +1,58 @@
+//
+//  PlayingCard.m
+//  Matchismo
+//
+//  Created by Mauro Oviedo on 7/9/13.
+//  Copyright (c) 2013 Mauro Oviedo. All rights reserved.
+//
+
+#import "PlayingCard.h"
+
+@interface PlayingCard()
+
+@end
+
+@implementation PlayingCard
+@synthesize suit = _suit; // because we provide setter AND getter
+
++ (NSArray *)validSuits
+{
+    return @[@"♥",@"♦",@"♠",@"♣"];
+}
+
++ (NSArray *)rankStrings
+{
+    return @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K"];
+}
+
++ (NSUInteger)maxRank
+{
+    return [self rankStrings].count - 1;
+}
+
+- (NSString *)contents
+{
+    NSArray * rankString = [PlayingCard rankStrings];
+    return [rankString[self.rank] stringByAppendingString:self.suit];
+}
+
+- (void)setSuit:(NSString *)suit
+{
+    if ([@[@"♥",@"♦",@"♠",@"♣"] containsObject:suit]) {
+        _suit = suit;
+    }
+}
+
+- (NSString *)suit
+{
+    return _suit ? _suit : @"?";
+}
+
+- (void)setRank:(NSUInteger)rank
+{
+    if (rank <= [PlayingCard maxRank]) {
+        _rank = rank;
+    }
+}
+
+@end
