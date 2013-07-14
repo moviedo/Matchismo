a76ff3bacbaa2b4a8fd69f6c24484073a4a1d4fa
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2013-07-14 16:42:02 -0400

    Added Class PlayingCardDeck.

---
+++ Matchismo/PlayingCardDeck.h
@@ -0,0 +1,14 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Mauro Oviedo on 7/9/13.
+//  Copyright (c) 2013 Mauro Oviedo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
