5601a68251e3c04b6938222a393a8f34155f552b
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2013-07-16 21:32:48 -0400

    Initial commit for Model Class CardMatchingGame.

---
+++ Matchismo/Models/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Mauro Oviedo on 7/16/13.
+//  Copyright (c) 2013 Mauro Oviedo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Matchismo/Models/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by Mauro Oviedo on 7/16/13.
+//  Copyright (c) 2013 Mauro Oviedo. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
