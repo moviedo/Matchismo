cc6a94c0870e381a5fce327b2a1f72357c9f2f4f
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2013-07-26 17:06:29 -0400

    Updated Card interface by adding Boolean properties.

--- Matchismo/Models/Card.h
+++ Matchismo/Models/Card.h
@@ -12,6 +12,9 @@
 
 @property (strong, nonatomic) NSString *contents;
 
+@property (nonatomic, getter = isFaceUp) BOOL faceUp;
+@property (nonatomic, getter = isUnplayable) BOOL unplayable;
+
 - (int)match:(NSArray *)otherCards;
 
 @end
