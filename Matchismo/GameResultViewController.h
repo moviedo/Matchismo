eb63aff179295a073943e6a3758d4d0f47111b02
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2013-08-06 10:00:22 -0400

    Updated storyboard to inculde tab view and 2nd VC, game results.

---
+++ Matchismo/GameResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameResultViewController.h
+//  Matchismo
+//
+//  Created by Mauro Oviedo on 8/4/13.
+//  Copyright (c) 2013 Mauro Oviedo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GameResultViewController : NSViewController
+
+@end
---
+++ Matchismo/GameResultViewController.m
@@ -0,0 +1,27 @@
+//
+//  GameResultViewController.m
+//  Matchismo
+//
+//  Created by Mauro Oviedo on 8/4/13.
+//  Copyright (c) 2013 Mauro Oviedo. All rights reserved.
+//
+
+#import "GameResultViewController.h"
+
+@interface GameResultViewController ()
+
+@end
+
+@implementation GameResultViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
