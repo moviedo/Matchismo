707ff1864337269838af6d73c692035df6c4b690
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2013-08-07 11:09:56 -0400

    Added implementation for sorting game results.

--- Matchismo/GameResultViewController.m
+++ Matchismo/GameResultViewController.m
@@ -12,6 +12,7 @@
 @interface GameResultViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextView *display;
+@property (nonatomic) SEL sortSelector;
 
 @end
 
@@ -21,7 +22,7 @@
 {
     NSString *displayText = @"";
     
-    for (GameResult *gameResult in [GameResult allGameResults]) {
+    for (GameResult *gameResult in [[GameResult allGameResults] sortedArrayUsingSelector:self.sortSelector]) {
         displayText = [displayText stringByAppendingFormat:@"Score: %d (%@, %0g)\n", gameResult.score, gameResult.start, round(gameResult.duration)];
     }
     self.display.text = displayText;
@@ -32,6 +33,35 @@
 {
     [super viewWillAppear:animated];
     [self updateUI];
+}
+
+
+@synthesize sortSelector = _sortSelector;  // because we implement BOTH setter and getter
+
+// return default sort selector if none set (by score)
+- (SEL)sortSelector
+{
+    if (!_sortSelector) _sortSelector = @selector(compareScoreToGameResult:);
+    return _sortSelector;
+}
+
+// update the UI when changing the sort selector
+- (void)setSortSelector:(SEL)sortSelector
+{
+    _sortSelector = sortSelector;
+    [self updateUI];
+}
+
+- (IBAction)sortByDate {
+    self.sortSelector = @selector(compareEndDateToGameResult:);
+}
+
+- (IBAction)sortByScore {
+    self.sortSelector = @selector(compareScoreToGameResult:);
+}
+
+- (IBAction)sortByDuration {
+    self.sortSelector = @selector(compareDurationToGameResult:);
 }
 
 @end
