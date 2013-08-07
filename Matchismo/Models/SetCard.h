6d8005456144a9162e3518c3d68c8460592dc351
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2013-08-07 15:30:21 -0400

    Initial commit for Model SetCard.

---
+++ Matchismo/Models/SetCard.h
@@ -0,0 +1,13 @@
+//
+//  SetCard.h
+//  Matchismo
+//
+//  Created by Mauro Oviedo on 8/7/13.
+//  Copyright (c) 2013 Mauro Oviedo. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface SetCard : Card
+
+@end
---
+++ Matchismo/Models/SetCard.m
@@ -0,0 +1,13 @@
+//
+//  SetCard.m
+//  Matchismo
+//
+//  Created by Mauro Oviedo on 8/7/13.
+//  Copyright (c) 2013 Mauro Oviedo. All rights reserved.
+//
+
+#import "SetCard.h"
+
+@implementation SetCard
+
+@end
