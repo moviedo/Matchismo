2a1cf898f7d6d9a6ca80729ef4c666223e22bf82
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2013-08-07 14:50:30 -0400

    Initial commit for Set ViewController.

---
+++ Matchismo/SetGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetGameViewController.h
+//  Matchismo
+//
+//  Created by Mauro Oviedo on 8/7/13.
+//  Copyright (c) 2013 Mauro Oviedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetGameViewController : UIViewController
+
+@end
---
+++ Matchismo/SetGameViewController.m
@@ -0,0 +1,27 @@
+//
+//  SetGameViewController.m
+//  Matchismo
+//
+//  Created by Mauro Oviedo on 8/7/13.
+//  Copyright (c) 2013 Mauro Oviedo. All rights reserved.
+//
+
+#import "SetGameViewController.h"
+
+@interface SetGameViewController ()
+
+@end
+
+@implementation SetGameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
