e8478dbe1049d00a821a8bee37a9ee996d1cff07
Mauro Oviedo <mauro.a.oviedo@gmail.com>
2013-08-16 15:51:40 -0400

    Implemented init method.

--- Matchismo/Models/SetCardDeck.m
+++ Matchismo/Models/SetCardDeck.m
@@ -7,7 +7,37 @@
 //
 
 #import "SetCardDeck.h"
+#import "SetCard.h"
 
 @implementation SetCardDeck
+
+- (id)init
+{
+    self = [super init];
+    
+    if (self) {
+        
+        for (NSString *symbol in [SetCard validSymbols]) {
+            SetCard *card = [[SetCard alloc] init];
+            
+            for (NSNumber *shadingValue in [SetCard validShadings]) {
+                card.shading = shadingValue;
+                
+                for (NSNumber *color in [SetCard validColors]) {
+                    card.color = color;
+                    
+                    for (NSUInteger numberOfSymbol = 1; numberOfSymbol <= [SetCard maxNumberOfSymbols]; numberOfSymbol++) {
+                        card.numberOfSymbols = numberOfSymbol;
+                        
+                    }
+                }
+            }
+            
+            [self addCard:card atTop:YES];
+        }
+    }
+    
+    return self;
+}
 
 @end
