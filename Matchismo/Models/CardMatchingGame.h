1d853848b76f07d7f3a8b043b8c8e1de7c2e5000
moviedo <mauro.o.2006@gmail.com>
2013-09-23 10:56:04 -0400

    Updated interface to reflect inheritance from Abstract parent class.

--- Matchismo/Models/CardMatchingGame.h
+++ Matchismo/Models/CardMatchingGame.h
@@ -7,27 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "Deck.h"
+#import "AbstractCardMatchingGame.h"
 
 
-@interface CardMatchingGame : NSObject
+@interface CardMatchingGame : AbstractCardMatchingGame
 
-// designated initializer
-- (id)initWithCardCount:(NSUInteger)cardCount
-              usingDeck:(Deck *)deck;
+- (void)flipCardAtIndex:(NSUInteger)index; //implementing abstract
 
-- (void)flipCardAtIndex:(NSUInteger)index; //abstract
-- (Card *)cardAtIndex:(NSUInteger)index;
-
-@property (nonatomic, readonly) int score;
-@property (nonatomic, readonly) int lastMove;// Output will be either CARD_FLIP, CARD_MATHCED or CARD_MISMATCH
-@property (strong, nonatomic) NSArray *positionsOfLastCardsPlayed;
+@property (nonatomic, readonly) int lastMove;//abstract
+@property (nonatomic, readonly) int score; //implementing abstract
 
 @end
 
-// enum declaration 
+// enum declaration
 enum {
-    CARD_SET_FLIP       = 1,
-    CARD_SET_MATHCED    = 2,
-    CARD_SET_MISMATCH   = 4
+    PLAYING_CARD_FLIP       = FLIP_COST,
+    PLAYING_CARD_MATHCED    = MATCH_BONUS,
+    PLAYING_CARD_MISMATCH   = MISMATCH_PENALTY
 };
