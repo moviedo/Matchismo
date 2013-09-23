0fc57b167c31713b7012c1593834196ed3cee399
moviedo <mauro.o.2006@gmail.com>
2013-09-22 22:11:59 -0400

    Added more static enum to interface.

--- Matchismo/Models/AbstractCardMatchingGame.h
+++ Matchismo/Models/AbstractCardMatchingGame.h
@@ -6,16 +6,29 @@
 //  Copyright (c) 2013 Mauro Oviedo. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import "AbstractCardMatchingGame.h"
+@interface AbstractCardMatchingGame : NSObject
 
+// designated initializer
+- (id)initWithCardCount:(NSUInteger)cardCount
+              usingDeck:(Deck *)deck;
 
-@interface CardMatchingGame : NSObject
-
+- (Card *)cardAtIndex:(NSUInteger)index;
 - (void)flipCardAtIndex:(NSUInteger)index; //abstract
 
-@property (nonatomic, readonly) int score;
-@property (nonatomic, readonly) int lastMove;// Output will be either CARD_FLIP, CARD_MATHCED or CARD_MISMATCH
-@property (strong, nonatomic) NSArray *positionsOfLastCardsPlayed;
+// Output will be either CARD_FLIP, CARD_MATHCED or CARD_MISMATCH
+@property (nonatomic, readonly) int lastMove;//abstract
+@property (nonatomic, readonly) int score; //abstract
+@property (strong, nonatomic) NSArray *positionsOfLastCardsPlayed; //abstract
 
 @end
+
+// enum declaration
+enum {
+    FLIP_COST           = 1,
+    MISMATCH_PENALTY    = 2,
+    MATCH_BONUS         = 4,
+    CARD_SET_FLIP       = FLIP_COST,
+    CARD_SET_MATHCED    = MATCH_BONUS,
+    CARD_SET_MISMATCH   = MISMATCH_PENALTY
+};
+
