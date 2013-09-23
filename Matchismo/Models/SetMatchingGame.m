676078bd32f27263cd403815e5edb432248f1e0d
moviedo <mauro.o.2006@gmail.com>
2013-09-23 18:16:46 -0400

    Implemented all abstract methods from abstract parent class.

--- Matchismo/Models/SetMatchingGame.m
+++ Matchismo/Models/SetMatchingGame.m
@@ -8,6 +8,70 @@
 
 #import "SetMatchingGame.h"
 
+@interface SetMatchingGame ()
+
+@property (strong, nonatomic, readwrite) NSMutableArray *cards;
+
+@property (nonatomic, readwrite) int lastMove;
+@property (nonatomic, readwrite) int score;
+
+@end
+
 @implementation SetMatchingGame
+
+- (void)flipCardAtIndex:(NSUInteger)index
+{
+    Card *card =[self cardAtIndex:index];
+    NSNumber *cardFlipped = [[NSNumber alloc] initWithInt:index];
+    self.positionsOfLastCardsPlayed = @[cardFlipped];
+    self.lastMove = FLIP_COST;
+    
+    NSMutableArray *otherCards = [[NSMutableArray alloc] init];
+    
+    if (!card.isUnplayable) {
+        if (!card.isFaceUp) {
+            
+            for (Card* otherCard in self.cards) {
+                if (otherCard.isFaceUp && !otherCard.isUnplayable) {
+                    [otherCards addObject:otherCard];
+                    
+                    if (otherCards.count == 2) {
+                        int matchScore = [card match:[otherCards copy]];
+                        
+                        if (matchScore) {
+                            for (Card *matchedCard in otherCards) {
+                                matchedCard.unplayable = YES;
+                            }
+                            card.unplayable = YES;
+                            
+                            matchScore = matchScore * MATCH_BONUS;
+                            self.lastMove = MATCH_BONUS;
+                            self.score += matchScore;
+                        }
+                        else {
+                            for (Card *matchedCard in otherCards) {
+                                matchedCard.faceUp = NO;
+                            }
+                            
+                            self.lastMove = MISMATCH_PENALTY;
+                            self.score -= MISMATCH_PENALTY;
+                            
+                        }
+                        
+                        //Add card positions to the property
+                        NSNumber *otherCardPlayed_1 = [[NSNumber alloc] initWithInt:[self.cards indexOfObject:otherCards[0]]];
+                        NSNumber *otherCardPlayed_2 = [[NSNumber alloc] initWithInt:[self.cards indexOfObject:otherCards[1]]];
+                        self.positionsOfLastCardsPlayed = [[NSArray alloc] initWithObjects:cardFlipped, otherCardPlayed_1, otherCardPlayed_2, nil];
+                    }
+                }
+            }
+            
+            self.score -= FLIP_COST;
+        }
+    }
+    
+    card.faceUp = !card.isFaceUp;
+}
+
 
 @end
