1b6345bf80c0aefd51cb50d6f3dfbe43d3386fd1
moviedo <mauro.o.2006@gmail.com>
2013-09-22 21:57:55 -0400

    Updated interface with more abstract properties.

--- Matchismo/Models/AbstractCardMatchingGame.h
+++ Matchismo/Models/AbstractCardMatchingGame.h
@@ -7,27 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "Deck.h"
+#import "AbstractCardMatchingGame.h"
 
 
 @interface CardMatchingGame : NSObject
 
-// designated initializer
-- (id)initWithCardCount:(NSUInteger)cardCount
-              usingDeck:(Deck *)deck;
-
 - (void)flipCardAtIndex:(NSUInteger)index; //abstract
-- (Card *)cardAtIndex:(NSUInteger)index;
 
 @property (nonatomic, readonly) int score;
 @property (nonatomic, readonly) int lastMove;// Output will be either CARD_FLIP, CARD_MATHCED or CARD_MISMATCH
 @property (strong, nonatomic) NSArray *positionsOfLastCardsPlayed;
 
 @end
-
-// enum declaration
-enum {
-    CARD_SET_FLIP       = 1,
-    CARD_SET_MATHCED    = 2,
-    CARD_SET_MISMATCH   = 4
-};
