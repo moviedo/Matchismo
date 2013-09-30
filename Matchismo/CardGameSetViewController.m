8ec839b7eb37a4b270f99e4470697c542029aea8
moviedo <mauro.o.2006@gmail.com>
2013-09-30 18:09:51 -0400

    Reimplemented controller with oop SetMatchingGame.

--- Matchismo/CardGameSetViewController.m
+++ Matchismo/CardGameSetViewController.m
@@ -9,25 +9,22 @@
 #import "CardGameSetViewController.h"
 #import "SetCard.h"
 #import "SetCardDeck.h"
-#import "CardMatchingGame.h"
+#import "SetMatchingGame.h"
 
 @interface CardGameSetViewController ()
 
-@property (strong, nonatomic) CardMatchingGame *game;
+@property (strong, nonatomic) SetMatchingGame *game;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 
 @end
 
 @implementation CardGameSetViewController
 
-- (CardMatchingGame *)game
+- (SetMatchingGame *)game
 {
     if (!_game) {
-        // Set game mode
-        int gameMode = 1;
-        _game = [[CardMatchingGame alloc]initWithCardCount:self.cardButtons.count
-                                                 usingDeck:[[SetCardDeck alloc] init]
-                                              withGameMode:gameMode];
+        _game = [[SetMatchingGame alloc]initWithCardCount:self.cardButtons.count
+                                                 usingDeck:[[SetCardDeck alloc] init]];
     }
     
     return _game;
@@ -35,7 +32,6 @@
 
 - (void)updateUI
 {
-    NSArray *playedCards = @[];
     for (UIButton *cardButton in self.cardButtons) {
         Card *card = [self.game cardAtIndex:[self.cardButtons indexOfObject:cardButton]];
         NSAttributedString *setGameAttributedString = nil;
@@ -62,7 +58,6 @@
         
         if (cardButton.selected) {
             [cardButton setBackgroundColor:[UIColor lightGrayColor]];
-            playedCards = [playedCards arrayByAddingObject:card];
         }
         else {
             [cardButton setBackgroundColor:nil];
@@ -70,16 +65,66 @@
     }
     
     // Update last move
-    if (!self.game.lastMove) {
-        self.lastMove = [[NSAttributedString alloc] initWithString:@"Last Move"];
-    }
-    else {
-       
-        self.lastMove = [CardGameSetViewController formatNSAttributedStringForLastMove:self.game.lastMove
-                                                                             withCards:playedCards];
-    }
+    [self updateLastMove];
+    
     // Update score
     self.score = self.game.score;
+}
+
+- (void)updateLastMove
+{
+    NSArray *positionsOfLastCardsPlayed = self.game.positionsOfLastCardsPlayed;
+    NSAttributedString *period = [[NSAttributedString alloc] initWithString:@"."];
+    
+    if (self.game.lastMove == SET_CARD_FLIP) {
+        UIButton *setCardButton = [self.cardButtons objectAtIndex:[[positionsOfLastCardsPlayed lastObject] integerValue]];
+        
+        if (setCardButton.isSelected) {
+            
+            NSMutableAttributedString *temp = [[NSMutableAttributedString alloc] initWithAttributedString:[setCardButton currentAttributedTitle]];
+            [temp appendAttributedString:period];
+            
+            [temp insertAttributedString:[[NSAttributedString alloc] initWithString:@"Selected "]
+                                 atIndex:0];
+            
+            self.lastMove = [temp copy];
+        }
+    }
+    else if (self.game.lastMove == SET_CARD_MATHCED || self.game.lastMove == SET_CARD_MISMATCH) {
+        UIButton *firstCard = [self.cardButtons objectAtIndex:[positionsOfLastCardsPlayed[0] integerValue]];
+        UIButton *secondCard = [self.cardButtons objectAtIndex:[positionsOfLastCardsPlayed[1] integerValue]];
+        UIButton *thirdCard = [self.cardButtons objectAtIndex:[positionsOfLastCardsPlayed[2]integerValue]];
+        
+        NSAttributedString *ampersand = [[NSAttributedString alloc] initWithString:@" & "];
+        NSAttributedString *spacing = [[NSAttributedString alloc] initWithString:@" "];
+        
+        //Add first attributed string, button title
+        NSMutableAttributedString *temp = [[NSMutableAttributedString alloc] initWithAttributedString:[firstCard currentAttributedTitle]];
+        
+        //Add second attributed string, button title, and spacing
+        [temp appendAttributedString:spacing];
+        [temp appendAttributedString:[secondCard currentAttributedTitle]];
+        
+        //Add third attributed string, button title, spacing and period
+        [temp appendAttributedString:ampersand];
+        [temp appendAttributedString:[thirdCard currentAttributedTitle]];
+        [temp appendAttributedString:period];
+        
+        if (self.game.lastMove == SET_CARD_MATHCED) {
+            
+            [temp insertAttributedString:[[NSAttributedString alloc] initWithString:@"Matched "]
+                                 atIndex:0];
+            
+        }
+        else {
+            
+            [temp insertAttributedString:[[NSAttributedString alloc] initWithString:@"Mismatched "]
+                                 atIndex:0];
+            
+        }
+        self.lastMove = [temp copy];
+        
+    }
 }
 
 - (void)setCardButtons:(NSArray *)cardButtons
@@ -130,34 +175,6 @@
 {
     [super viewWillAppear:animated];
     [self updateUI];
-}
-
-+ (NSAttributedString *)formatNSAttributedStringForLastMove:(NSString *)string
-                                            withCards:(NSArray *)playedCards
-{
-    NSMutableAttributedString *lastMove = [[[NSAttributedString alloc] initWithString:string] mutableCopy];
-    
-    for (SetCard *card in playedCards) {
-        NSRange sybmolRange = [string rangeOfString:card.contents];
-        
-        [lastMove setAttributes:[CardGameSetViewController createAttributeDictionary:card]
-                          range:sybmolRange];
-    }
-    
-    return lastMove;
-}
-
-+ (NSDictionary *)createAttributeDictionary:(SetCard *)card
-{
-    UIColor *color = [CardGameSetViewController translateColorFromModel:(SetCard *)card];
-    
-    NSDictionary *symbolAttributes = @{
-                                       NSForegroundColorAttributeName : color,
-                                       NSStrokeWidthAttributeName : @-5,
-                                       NSStrokeColorAttributeName : [color colorWithAlphaComponent:1.0]
-                                       };
-    
-    return symbolAttributes;
 }
 
 @end
